633bdcaf0e8f3c3b85c8714c928a40a664a21940
Matt Chatterley <matt@code.je>
2022-05-05 12:40:15 +0100

    Update to 6.4.0 and switch to Microsoft.Data.SqlClient

--- src/app/Providers/SqlServerConnectionProvider.cs
+++ src/app/Providers/SqlServerConnectionProvider.cs
@@ -6,7 +6,7 @@
     using System.Data;
     using System.Text;
     using System.Threading.Tasks;
-    using System.Data.SqlClient;
+    using Microsoft.Data.SqlClient;
 
     public class SqlServerConnectionProvider : IDbConnectionProvider
     {
